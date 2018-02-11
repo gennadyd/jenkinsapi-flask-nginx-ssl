0934e1729a56992334526192e0a620908ac8b6b3
gennadyd <gennady.davidov@gmail.com>
2018-02-11 13:03:23 +0200

    Add files via upload

---
+++ roles/nginx/files/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo ">> DOCKER-ENTRYPOINT: GENERATING SSL CERT"
+
+cd /opt/ssl/
+openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+openssl rsa -passin pass:x -in server.pass.key -out server.key
+rm server.pass.key
+openssl req -new -key server.key -out server.csr -subj "/C=IL/ST=Netanya/L=Netanya/O=myorg.info/OU=myorg.info/CN=local.myorg.info"
+openssl x509 -req -sha256 -days 300065 -in server.csr -signkey server.key -out server.crt
+
+echo ">> DOCKER-ENTRYPOINT: GENERATING SSL CERT ... DONE"
+echo ">> DOCKER-ENTRYPOINT: EXECUTING CMD"
+
+exec "$@"
